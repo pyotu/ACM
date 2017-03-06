11719f7f8b551aba5cc7e9c8c098c298d99939cb
pyotu <schen71@toromail.csudh.edu>
2017-03-06 15:50:27 -0800

    add solutions

---
+++ ACM.BL/Product.cs
@@ -0,0 +1,59 @@
+﻿using Acme.Common;
+
+namespace ACM.BL
+{
+    public class Product : EntityBase, ILoggable
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            this.ProductId = productId;
+        }
+
+        public decimal? CurrentPrice { get; set; }
+        public int ProductId { get; private set; }
+        public string ProductDescription { get; set; }
+
+        private string _productName;
+
+        public string ProductName
+        {
+            get {
+                return _productName.InsertSpaces();
+            }
+            set { _productName = value; }
+        }
+        
+
+        /// <summary>
+        /// Validates the product data.
+        /// </summary>
+        /// <returns></returns>
+        public override bool Validate()
+        {
+            bool isValid = !string.IsNullOrWhiteSpace(ProductName);
+
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
+
+        public override string ToString()
+        {
+            return ProductName;
+        }
+
+        public string Log()
+        {
+            var logString = ProductId + ": " +
+                            ProductName + " " +
+                            "Detail: " + ProductDescription + " " +
+                            "Status: " + EntityState;
+            return logString;
+        }
+    }
+}
